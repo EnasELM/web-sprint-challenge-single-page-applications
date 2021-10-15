668ab5f273bdcb9b6541554baa88fe5bd1031c5c
Enas <enaselmahdwi@gmail.com>
2021-10-15 16:55:06 -0600

    updata

--- src/HomePage.js
+++ src/HomePage.js
@@ -122,6 +122,7 @@ function HomePage() {
         }
  return(
    <div>
+       <button onClick={pizzaOrderShop}>Order NOW</button>
        <h1>Friends App</h1>
     {/* <Pizza
       values={formValues}
@@ -134,11 +135,12 @@ function HomePage() {
         {/* <Route path="/pizza-form">
            <Pizza items={pizzaOrder} />
       </Route> */}
+      
        <img src='https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHBpenphfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
        alt='pizza'/>
-       {/* <button onClick={pizzaOrderShop}>Order NOW</button> */}
        
-       <button onClick={pizzaForm } id = "order-pizza">Build Your pizza</button>
+       
+       {/* <button onClick={pizzaForm } id = "order-pizza">Build Your pizza</button> */}
    </div>  
  )
 }
